d7d50da180fe0a85173717cddc12347ef75bbe41
Marcos <marcos.aguilera.7.13@gmail.com>
2020-04-21 16:11:27 -0300

    Agregando cambios de demi

--- src/main/java/modelo/DecoradorCable.java
+++ src/main/java/modelo/DecoradorCable.java
@@ -2,19 +2,19 @@ package modelo;
 
 public class DecoradorCable extends DecoradorServicio {
 
-	public DecoradorCable() {
-		// TODO Auto-generated constructor stub
+	
+
+	public DecoradorCable(IServicio servicioEncapsulado) {
+		super(servicioEncapsulado);
 	}
 
 	public double getCosto() {
-		// TODO Auto-generated method stub
 		return 250;
 	}
 
 	@Override
 	public String detalle() {
-		// TODO Auto-generated method stub
 		return super.detalle() + " Tv Cable: $" + this.getCosto() + "\n";
 	}
-	
+
 }
