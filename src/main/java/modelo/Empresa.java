9f75fb75dc24d2f0090d0725491a411c004af116
MatiasC248 <MatiasC248@users.noreply.github.com>
2020-04-22 19:21:55 -0300

    Busquedas de Empresa completadas

--- src/main/java/modelo/Empresa.java
+++ src/main/java/modelo/Empresa.java
@@ -12,17 +12,66 @@ public class Empresa {
 		this.nombre = nombre;
 	}
 	
+	//MAL
 	public void addContrato(Contrato contrato) {
 		contratos.add(contrato);
 	}
-	
+	//
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public ArrayList<Contrato> getContratos() {
+		return contratos;
+	}
+
 	public Contrato buscaContrato(int id) {
+		boolean encontre = false;
 		Contrato contrato = null;
-		
 		Iterator<Contrato> it = contratos.iterator();
-		
-		
+		while(it.hasNext() && encontre) {
+			contrato = it.next();
+			if(id==contrato.getIdContrato())
+				encontre = true;
+		}
+		if(!encontre)
+			contrato = null;
 		return contrato;
+	}
+	
+	public Contrato buscaContrato(String calle, int numero) {
+		boolean encontre = false;
+		Domicilio domicilio = new Domicilio(calle,numero);
+		Contrato contrato = null;
+		Iterator<Contrato> it = contratos.iterator();
+		while(it.hasNext() && encontre) {
+			contrato = it.next();
+			if(contrato.getDomicilio().equals(domicilio))
+				encontre = true;
+		}
+		if(!encontre)
+			contrato = null;
+		return contrato;
+	}
+	
+	public ArrayList<Contrato> buscaContratosTitular(String nombre, int dni) {
+		ArrayList<Contrato> contratosTitular = new ArrayList<Contrato>();
+		Contrato contrato = null;
+		Persona titular = null;
+		Iterator<Contrato> it = contratos.iterator();
+		while(it.hasNext()) {
+			contrato = it.next();
+			titular = contrato.getTitular();
+			if(titular.getNombre().equals(nombre) && (titular.getDni()==dni))
+				contratosTitular.add(contrato);
+		}
+		if(contratosTitular.isEmpty())
+			contratosTitular = null;
+		return contratosTitular;
 	}
 	
 	public String reporte() {
