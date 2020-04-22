cfc56ebdd215f60ebfe9ead5ea2cb9466d6b50d6
Denmigod <58298538+Denmigod@users.noreply.github.com>
2020-04-22 16:45:03 -0300

    add ContratoFactory

---
+++ src/main/java/modelo/ContratoFactory.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class ContratoFactory {
+
+	public ContratoFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Contrato getContrato(Persona titular, Domicilio domicilio, IServicio paqueteServicios) {
+		if (titular!=null)
+				return new Contrato(titular,domicilio,paqueteServicios);
+		return null;
+	}
+}
