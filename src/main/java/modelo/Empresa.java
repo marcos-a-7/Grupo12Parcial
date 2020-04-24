b6c54ecc2d07fe389cf8ddc4098521b19346b867
MatiasC248 <MatiasC248@users.noreply.github.com>
2020-04-24 17:41:40 -0300

    DetallesUpdate

--- src/main/java/modelo/Empresa.java
+++ src/main/java/modelo/Empresa.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import clientes.Domicilio;
+import clientes.Persona;
 import clientes.PersonaFactory;
 import excepciones.ImposibleCrearPaqueteException;
 import excepciones.MedioPagoInvalidoException;
