e7a239260b47d69e60964bd42d80a7a6e4d220de
Marcos <marcos.aguilera.7.13@gmail.com>
2020-06-13 19:37:39 -0300

    busca contrato y elimina contrato en persona

--- src/main/java/clientes/Persona.java
+++ src/main/java/clientes/Persona.java
@@ -47,16 +47,82 @@ public abstract class Persona implements Cloneable {
 
 	}
 	
-	/* hacer funciones por domicilio y por id de contrato
-	public Contrato buscaContrato() {
-		//hacer
-		return null;
+	/**
+	 * Busca un contrato a traves de un id<br>
+	 * <b>Pre: </b> no tiene<br>
+	 * <b>Post: </b> devolvera el contrato de dicho id ingresado, si no lo encuentra
+	 * devolvera null<br>
+	 * 
+	 * @param id : es el id el cual esta asociado a un contrato<br>
+	 * @return retorna un contrato o null <br>
+	 */
+	public Contrato buscaContrato(int id) {
+		boolean encontre = false;
+		Contrato contrato = null;
+		Iterator<Contrato> it = contratos.iterator();
+		while (it.hasNext() && !encontre) {
+			contrato = it.next();
+			if (id == contrato.getIdContrato())
+				encontre = true;
+		}
+		if (!encontre)
+			contrato = null;
+		return contrato;
+	}
+
+	/**
+	 * Busca un contrato a traves de una calle y el numero de direccion<br>
+	 * <b>Pre: </b> Debe ingresar una calle no nula<br>
+	 * <b>Post: </b> devolvera el contrato correspondiente a la calle y numero
+	 * ingresado, si no lo encuentra devolvera null<br>
+	 * 
+	 * @param calle  : La calle del domicilio de la persona
+	 * @param numero : El numero de la calle del domicilio de la persona
+	 * @return retorna un contrato o null<br>
+	 */
+	public Contrato buscaContrato(String calle, int numero) {
+		boolean encontre = false;
+		Domicilio domicilio = null;
+		Contrato contrato = null;
+		Iterator<Contrato> it = contratos.iterator();
+		while (it.hasNext() && !encontre) {
+			contrato = it.next();
+			domicilio = contrato.getDomicilio();
+			if (domicilio.getCalle().equals(calle) && (domicilio.getNumero() == numero))
+				encontre = true;
+		}
+		if (!encontre)
+			contrato = null;
+		return contrato;
 	}
 	
-	public void borraContrato() {
-		//hacer
-	}
+	/**
+	 * Elimina un contrato segun el id <b>Pre: </b> no tiene<br>
+	 * <b>Post: </b> remueve el contrato si el id corresponde a uno existente, de lo
+	 * contrario no realiza ninguna accion<br>
+	 * 
+	 * @param id el id del contrato a eliminar
 	 */
+	public void eliminaContrato(int id) {
+		Contrato contrato = this.buscaContrato(id);
+		if (contrato != null)
+			this.contratos.remove(contrato);
+	}
+
+	/**
+	 * Elimina un contrato segun la calle y el numero de domicilio <b>Pre: </b> no
+	 * tiene<br>
+	 * <b>Post: </b> remueve el contrato si la calle y el numero de la calle
+	 * corresponden a uno existente, de lo contrario no realiza ninguna accion<br>
+	 * 
+	 * @param calle  : la calle del domicilio del titular<br>
+	 * @param numero : el numero del domicilio del titular <br>
+	 */
+	public void eliminaContrato(String calle, int numero) {
+		Contrato contrato = this.buscaContrato(calle, numero);
+		if (contrato != null)
+			this.contratos.remove(contrato);
+	}
 	
 	//esta funcion pasa una referencia a la persona para que la factura pueda cambiar en caso de que cambien los datos de la persona (ejemplo medio de pago)
 	public void facturar() {
